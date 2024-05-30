880c2433ccfa4f329adcf54777fef39789a1dbe7
LuccasSr <djluccasdaquebrada@gmail.com>
2024-05-29 21:37:14 -0300

    Novos Arquivos

---
+++ Javascript/eventos.js
@@ -0,0 +1,21 @@
+/* Eventos sao açoes disparadas pela interaçao dos usuarios na pagina.
+e o correto manejo desses eventos que tornam as paginas interativas e dinamicas.
+
+existem muitos eventos. Veja os mais utilizados:
+
+onclick = Disparado quando recebe um click do usuario.
+ondclick - Disparado quando clique duplo.
+onmouseover - Disparado quando o mouse esta sobre.
+onmouseout - Dispardo quando o mouse e movido para fora do elemento.
+onmousemove - Disparado quando o mouse é movido no elemento.
+onmousedown - Disparado quando o clique do botao foi pressionado.
+onmouseup - Disparado quando o clique do botao e liberado.
+onfocus - Disparado quando o elemento recebe o foco. valido para input,
+onchange - Disaparado quando existe uma mudança no conteudo. "ao mudar".
+onblur - Disparado quando o elemento perde o foco.
+onkeydown - Disparado quando uma tecla e pressionada e solta.
+onkeyup - Disparado quando uma tecla e solta sobre um elemento.
+onload - Disparado quando a pagina terminou de ser carregada. body.
+onresize - Disparado quando há um redimencionamento da janela.
+*/
+
